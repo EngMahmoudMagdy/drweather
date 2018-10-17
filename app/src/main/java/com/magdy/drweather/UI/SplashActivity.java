df96729b80a76e9081e885f23ecc72b85e47e356
Mahmoud Magdy <engmahmoudmagdy11@gmail.com>
2018-10-17 03:08:35 +0200

    first commit

---
+++ app/src/main/java/com/magdy/drweather/UI/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.magdy.drweather.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.magdy.drweather.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
